5b4bbe63e5afdc00e6972f0aebc723ba0c2f78d3
servitronjaque <pato_2391@hotmail.com>
2015-11-05 19:43:33 -0600

    BUeno

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require("http");
 function onRequest(request, response) {
   console.log("Peticion Recibida.");
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundito2jiji");
+  response.write("Hola Mundo0!!!");
   response.end();
 }
 
